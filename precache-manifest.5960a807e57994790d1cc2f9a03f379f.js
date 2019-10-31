f9e7faa991d67c98364183076fb81398da4cb228
Dmitriymush <powder322@gmail.com>
2019-10-31 04:31:28 +0200

    Updates

---
+++ precache-manifest.5960a807e57994790d1cc2f9a03f379f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "918b3e18faf5a77de6eab4616088bc7d",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "b7f14e9ce1a4a6981084",
+    "url": "/react_add-todo-form/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "9cf2461a171d9d308bff",
+    "url": "/react_add-todo-form/static/js/2.8d3b075d.chunk.js"
+  },
+  {
+    "revision": "b7f14e9ce1a4a6981084",
+    "url": "/react_add-todo-form/static/js/main.e163873f.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
